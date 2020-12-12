1d9b97c514798b5a0754759c2523aef9a6224795
tsukumijima <tsukumijima@users.noreply.github.com>
2020-12-13 07:58:21 +0900

    KakologController を作成

--- app/Http/Controllers/KakologController.php
+++ app/Http/Controllers/KakologController.php
@@ -2,9 +2,79 @@
 
 namespace App\Http\Controllers;
 
+use App\Kakolog;
 use Illuminate\Http\Request;
 
 class KakologController extends Controller
 {
-    //
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index(string $jikkyo_id, Request $request)
+    {
+        // クエリパラメータがあるか確認
+        if ($request->has(['format', 'starttime', 'endtime'])) {
+
+            // フォーマット
+            $format = strtolower($request->input('format'));
+
+            // 取得開始/終了時刻のタイムスタンプ
+            $starttime = $request->input('starttime');
+            $endtime = $request->input('endtime');
+
+            // 有効なタイムスタンプでない場合はエラー
+            if (!$this->isValidTimeStamp($starttime) or !$this->isValidTimeStamp($endtime)) {
+                return [
+                    'error' => 'The start time or end time is invalid.',
+                ];
+            }
+
+            // 生の過去ログを取得
+            // xml ヘッダはついていない
+            $kakolog_raw = Kakolog::getKakolog($jikkyo_id, intval($starttime), intval($endtime));
+
+            // 指定された時間範囲の過去ログが存在しない
+            if ($kakolog_raw === false) {
+                return [
+                    'error' => 'The kakolog in the specified time range does not exist.',
+                ];
+            }
+
+            // XML または JSON にフォーマット
+            if ($format === 'xml') {
+                $kakolog = Kakolog::formatToXML($kakolog_raw);
+            } else if ($format === 'json') {
+                $kakolog = Kakolog::formatToJson($kakolog_raw);
+            } else {
+                // XML でも JSON でもなかったらエラー
+                return [
+                    'error' => 'The format must be xml or json.',
+                ];
+            }
+
+            // 過去ログを返す
+            return $kakolog;
+
+        } else {
+
+            // エラーを返す (JSON)
+            return [
+                'error' => 'The required parameter does not exist.',
+            ];
+        }
+    }
+
+
+    /**
+     * 有効なタイムスタンプかどうかを返す 
+     *
+     * @param mixed $timestamp タイムスタンプ
+     * @return boolean 有効なタイムスタンプかどうか
+     */
+    private function isValidTimeStamp($timestamp) {
+        // 0 以上で現在のタイムスタンプ以下の数値
+        return is_numeric($timestamp) and intval($timestamp) >= 0 and intval($timestamp) <= time();
+    }
 }
--- app/Kakolog.php
+++ app/Kakolog.php
@@ -6,5 +6,39 @@ use Illuminate\Database\Eloquent\Model;
 
 class Kakolog extends Model
 {
-    //
+    /**
+     * 過去ログを取得する
+     * 指定された開始時刻/終了時刻の xml ファイルがあれば生の過去ログデータを、なければ false を返す
+     *
+     * @param string $jikkyo_id 実況ID (ex: jk211)
+     * @param integer $starttime 取得を開始する時刻のタイムスタンプ
+     * @param integer $endtime 取得を終了する時刻のタイムスタンプ
+     * @return mixed 生の過去ログデータか false
+     */
+    public static function getKakolog(string $jikkyo_id, int $starttime, int $endtime)
+    {
+        //
+    }
+    
+
+    /**
+     * 生の過去ログデータを XML にフォーマットする
+     *
+     * @param string $kakolog_raw 生の過去ログデータ
+     * @return string XML にフォーマットした過去ログデータ
+     */
+    public static function formatToXML(string $kakolog_raw) {
+        //
+    }
+    
+
+    /**
+     * 生の過去ログデータを JSON にフォーマットする
+     *
+     * @param string $kakolog_raw 生の過去ログデータ
+     * @return string JSON にフォーマットした過去ログデータ
+     */
+    public static function formatToJSON(string $kakolog_raw) {
+        //
+    }
 }
